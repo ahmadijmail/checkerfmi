f83cb0e330d1118001bc3ad2dccb70daa454c6be
ahmadijmail <ahmadijmil2010@gmail.com>
2022-09-29 00:41:42 +0300

    update1

--- index.js
+++ index.js
@@ -1,56 +1,73 @@
 require("dotenv").config();
-const TelegramBot = require('node-telegram-bot-api')
-const express = require('express')
-const app = express()
+const TelegramBot = require("node-telegram-bot-api");
+const express = require("express");
+const app = express();
 const axios = require("axios");
 const { TOKEN } = process.env;
-let bot = new TelegramBot(TOKEN, { polling: true })
+let bot = new TelegramBot(TOKEN, { polling: true });
 let url = "https://api.ifreeicloud.co.uk";
 
-app.get("/", function(req,res){
-    res.send("working")
-})
+app.get("/", function (req, res) {
+  res.send("working");
+});
 
+app.listen(process.env.PORT);
 
-app.listen(process.env.PORT)
-
-
-
-bot.on('message', function (msg) {
-    console.log(msg.text);
-    let chatID = msg.chat.id;
-    let tex = msg.text
-
-    axios
-        .post(url, null, {
-            params: {
-                service: 4,
-                imei: tex,
-                key: "9S3-CK9-H4G-EWT-XKI-VO9-J9X-QZI",
-            },
-            
-        } , 
-        bot.sendMessage(chatID, "Please Wait ...")
-        ).then((res) => {
-
-            if(res.data.success==false){
-                bot.sendMessage(chatID, res.data.error)
-            }else {
-                console.log(res.data);
-                if(res.data.object.fmiOn == false){
-                    bot.sendMessage(chatID, 
-                        `IMEI: ${res.data.object.imei}
-FMI: OFF ✅ ` )
-                }
-
-                if(res.data.object.fmiOn == true){
-                    bot.sendMessage(chatID, 
-                        `IMEI: ${res.data.object.imei} 
-FMI: ON 🔴`)
-                }
-              
-
-            } })
-
-})
-
+bot.on("message", function (msg) {
+  console.log(msg.text);
+  let chatID = msg.chat.id;
+  let tex = msg.text;
+  let serviceid = tex.split(" ");
+  console.log(serviceid);
+  axios
+    .post(
+      url,
+      null,
+      {
+        params: {
+          service: serviceid[0] == "info" ? 120 : 4,
+          imei: serviceid[0] == "info" ? serviceid[1] : tex,
+          key: "9S3-CK9-H4G-EWT-XKI-VO9-J9X-QZI",
+        },
+      },
+      bot.sendMessage(chatID, "Please Wait ...")
+    )
+    .then((res) => {
+      if (serviceid[0] != "info") {
+        if (res.data.success == false) {
+          bot.sendMessage(chatID, res.data.error);
+        } else {
+          bot.sendMessage(
+            chatID,
+            `IMEI: ${res.data.object.imei}
+              FMI: ${
+res.data.object.fmiOn == false ? "FMI: OFF ✅ " : "FMI: ON 🔴"
+              } `
+          );
+        }
+      }
+      if (serviceid[0] == "info") {
+        console.log(res.data);
+        if (res.data.success == false) {
+          bot.sendMessage(chatID, res.data.error);
+        } else {
+          bot.sendMessage(
+            chatID,
+            `
+Model: ${res.data.object.modelDescription?res.data.object.modelDescription:res.data.object.model}
+IMEI: ${res.data.object.imei}
+IMEI2: ${res.data.object.imei2}
+Serial: ${res.data.object.imei}
+iCloud Lock: ${res.data.object.fmiON == true ? " ON 🔴 " : " OFF ✅ "} 
+iCloud Status: ${ res.data.object.lostMode == true? " Lost 🔴 " : " Clean ✅" } 
+blacklistStatus: ${res.data.object.blacklistStatus == "Clean"? "  Clean   ✅": "BlackListed 🔴 "}
+replacedStatus: ${res.data.object.replacedDevice}
+Warranty Status: ${res.data.object.warrantyStatus}
+Estimated Purchase Date: ${res.data.object.estimatedPurchaseDate}
+simLock: ${res.data.object.simLock == false ? "Unlocked" : "Locked"}   
+               `
+          );
+        }
+      }
+    });
+});
