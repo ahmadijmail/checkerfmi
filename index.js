3102fc7456ee5847305a9e40e806e71a2a9f555e
ahmadijmail <ahmadijmil2010@gmail.com>
2023-12-08 14:55:41 +0300

    remove cron

--- index.js
+++ index.js
@@ -2,17 +2,9 @@ require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 const { handleMessage } = require("./messageHandler");
 const { generateMainKeyboard } = require("./keyboardGenerator");
-const express = require('express');
-const { job } = require("./cron");
 
 const botToken = process.env.BOT_TOKEN;
 const bot = new TelegramBot(botToken, { polling: true });
-const app = express();
-const port = process.env.PORT || 3000; // You can set the port in your environment variables or default to 3000
-
-
-
-job.start(); // Start the cron job
 
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "✔️ Please choose Service:👇﻿", generateMainKeyboard());
