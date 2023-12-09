654c25510437af10198c93b6a665868d9703b9cc
ahmadijmail <ahmadijmil2010@gmail.com>
2023-12-10 02:26:41 +0300

    add endpoint

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 const { handleMessage } = require("./messageHandler");
 const { generateMainKeyboard } = require("./keyboardGenerator");
+const express = require('express');
 
 const botToken = process.env.BOT_TOKEN;
 const bot = new TelegramBot(botToken, { polling: true });
+
+// Set up the Express server
+const app = express();
+const port = process.env.PORT || 3000; // You can set the port in your environment variables or default to 3000
+
+// Start Express server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Define a route for the base URL
+app.get('/', (req, res) => {
+  res.send("It's working!"); // This message will be sent when someone hits the base URL
+});
+
 
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "✔️ Please choose Service:👇﻿", generateMainKeyboard());
