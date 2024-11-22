f6b3a6564e13756a365a165530c6180a0664e552
ahmadijmail <ahmadijmil2010@gmail.com>
2024-11-22 03:23:54 +0300

    chore: enable polling

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if (!authorizedUserId) {
   process.exit(1);
 }
 
-const bot = new TelegramBot(botToken);
+const bot = new TelegramBot(botToken, { polling: true });
 const app = express();
 const PORT = process.env.PORT || 3000;
 
